7440018f081189c81a996d7aef8e1379523d80a1
YeoseosBam <bsdav45@gmail.com>
2018-12-08 03:13:08 +0900

    Change file name & Add function > 가독성을 위해 파일 이름 변경 > "toggleUpdateInfo" 함수 추가

---
+++ clickEvent.js
@@ -0,0 +1,73 @@
+let nanase = document.getElementById('nanase');
+let nogi = document.getElementById('nogi');
+let yoda = document.getElementById('yoda');
+let asuka = document.getElementById('asuka');
+let kubo = document.getElementById('kubo');
+/*------------------------------------*/
+let member = "default"
+nanase.onclick = function(element) {
+  chrome.storage.sync.set({
+    key: "西野七瀬"
+  }, function() {
+    console.log('Vaule is set to ' + "西野七瀬");
+  })
+  chrome.storage.sync.get(['key'], function(result) {
+    console.log('Value currently is ' + result.key);
+  });
+}
+
+nogi.onclick = function(element) {
+  chrome.storage.sync.set({
+    key: "乃木坂"
+  }, function() {
+    console.log('Vaule is set to ' + "乃木坂");
+  })
+  chrome.storage.sync.get(['key'], function(result) {
+    console.log('Value currently is ' + result.key);
+  });
+}
+
+yoda.onclick = function(element) {
+  chrome.storage.sync.set({
+    key: "与田祐希"
+  }, function() {
+    console.log('Vaule is set to ' + "与田祐希");
+  })
+  chrome.storage.sync.get(['key'], function(result) {
+    console.log('Value currently is ' + result.key);
+  });
+}
+
+asuka.onclick = function(element) {
+  chrome.storage.sync.set({
+    key: "齋藤飛鳥"
+  }, function() {
+    console.log('Vaule is set to ' + "齋藤飛鳥");
+  })
+  chrome.storage.sync.get(['key'], function(result) {
+    console.log('Value currently is ' + result.key);
+  });
+}
+
+kubo.onclick = function(element) {
+  chrome.storage.sync.set({
+    key: "久保史绪里"
+  }, function() {
+    console.log('Vaule is set to ' + "久保史绪里");
+  })
+  chrome.storage.sync.get(['key'], function(result) {
+    console.log('Value currently is ' + result.key);
+  });
+}
+/*-----------------------------------*/
+function toggleUpdataInfo() {
+  var target=document.getElementById("updateInfo");
+  if(target.style.display=="none"){
+    target.style.display="block";
+    document.getElementById("t1").innerHTML="가리기"
+  }
+  else{
+    target.style.display="none";
+    document.getElementById("t1").innerHTML="업데이트 내역 보기"
+  }
+}
